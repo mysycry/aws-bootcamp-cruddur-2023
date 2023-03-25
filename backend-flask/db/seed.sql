cb72127ccbf5628f2da9bc3ce7a337e523d61c38
Birch Tree <josiasmichael@gmail.com>
2023-03-25 12:45:38 +0000

    implement dynamodb into our application

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,9 +1,10 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'MOCK'),
-  ('Michael Josias', 'josiasmichael@gmail.com', 'mysycry' ,'MOCK'),
-  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'MOCK');
+  /*('Andrew Brown', 'andrew@exampro.co', 'andrewbrown' ,'f73f4a05-a59e-468a-8a29-a1c39e7a1111'), */
+  ('Michael Josias', 'josiasmichael@gmail.com', 'mysycry' ,'64669d1f-2011-44c4-a7bd-0ba359787a17'),
+  ('Andrew Bayko', 'bayko@exampro.co', 'bayko' ,'f73f4b05-a59e-468b-8a29-a1c39e7a2222'),
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
