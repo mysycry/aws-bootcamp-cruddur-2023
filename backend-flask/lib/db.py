1485c49f26dafc5a1e7b272ed517f8e7b4300e62
Birch Tree <josiasmichael@gmail.com>
2023-04-09 16:19:09 +0000

    time fixes

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -24,7 +24,7 @@ class Db:
     return template_content
 
   def init_pool(self):
-    connection_url = os.getenv("CONNECTION_URL")
+    connection_url = os.getenv("LOCAL_CONNECTION_URL")
     self.pool = ConnectionPool(connection_url)
   # we want to commit data such as an insert
   # be sure to check for RETURNING in all uppercases
