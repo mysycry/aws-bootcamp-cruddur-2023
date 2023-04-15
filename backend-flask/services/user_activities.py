74850bc2e9344549c66cc8ccf33f4e9fe95dfa5c
Birch Tree <josiasmichael@gmail.com>
2023-04-15 21:16:40 +0000

    implement users edit page

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,38 +1,29 @@
-from datetime import datetime, timedelta, timezone
-from aws_xray_sdk.core import xray_recorder
+from lib.db import db
+#from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
   def run(user_handle):
-    # Xray -----
-    # segment = xray_recorder.begin_segment('user_activities')
+    #try:
     model = {
       'errors': None,
       'data': None
     }
-
-    now = datetime.now(timezone.utc).astimezone()
-
-
-
     if user_handle == None or len(user_handle) < 1:
       model['errors'] = ['blank_user_handle']
     else:
-      now = datetime.now()
-      results = [{
-        'uuid': 'f450db40-4f00-46db-8472-7f461d1016b0',
-        'handle':  'Michael Josias',
-        'message': 'Cloud is good. All the time.',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
+      print("else:")
+      sql = db.template('users','show')
+      results = db.query_object_json(sql,{'handle': user_handle})
       model['data'] = results
-
-    # subsegment = xray_recorder.begin_subsegment('mock-data')
-    # Xray -----
-    # dict = {
-    #  "now": now.isoformat(),
-    #  "results-size": len(model['data'])
-    # }
-    # subsegment.put_metadata('key', dict, 'namespace')
-
+      #subsegment = xray_recorder.begin_subsegment('mock-data')
+      ## xray ---
+      #dict = {
+      #  "now": now.isoformat(),
+      #  "results-size": len(model['data'])
+      #}
+      #subsegment.put_metadata('key', dict, 'namespace')
+      #xray_recorder.end_subsegment()
+    #finally:  
+    ##  # Close the segment
+    #  xray_recorder.end_subsegment()
     return model
