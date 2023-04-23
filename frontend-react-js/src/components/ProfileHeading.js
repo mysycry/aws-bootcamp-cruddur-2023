2fc9acd80f0d77c8b6fa59317bdfe6f3ce5810b3
Birch Tree <josiasmichael@gmail.com>
2023-04-23 20:40:19 +0000

    render avatar

--- frontend-react-js/src/components/ProfileHeading.js
+++ frontend-react-js/src/components/ProfileHeading.js
@@ -1,6 +1,8 @@
 import './ProfileHeading.css';
 import EditProfileButton from '../components/EditProfileButton';
 
+import ProfileAvatar from 'components/ProfileAvatar'
+
 export default function ProfileHeading(props) {
   const backgroundImage = 'url("https://assets.cloudmate.tech/banners/banner.jpg")';
   const styles = {
@@ -9,23 +11,21 @@ export default function ProfileHeading(props) {
     backgroundPosition: 'center',
   };
   return (
-  <div className='activity_feed_heading profile_heading'>
-    <div className='title'>{props.profile.display_name}</div>
-    <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
-    <div className="banner" style={styles} >
-      <div className="avatar">
-        <img src="https://assets.cloudmate.tech/avatars/heterochromia.jpg"></img>
+    <div className='activity_feed_heading profile_heading'>
+      <div className='title'>{props.profile.display_name}</div>
+      <div className="cruds_count">{props.profile.cruds_count} Cruds</div>
+      <div className="banner" style={styles} >
+        <ProfileAvatar id={props.profile.cognito_user_uuid} />
       </div>
-    </div>
-    <div className="info">
-      <div className='id'>
-        <div className="display_name">{props.profile.display_name}</div>
-        <div className="handle">@{props.profile.handle}</div>
+      <div className="info">
+        <div className='id'>
+          <div className="display_name">{props.profile.display_name}</div>
+          <div className="handle">@{props.profile.handle}</div>
+        </div>
+        <EditProfileButton setPopped={props.setPopped} />
       </div>
-      <EditProfileButton setPopped={props.setPopped} />
+      <div className="bio">{props.profile.bio}</div>
+  
     </div>
-    <div className="bio">{props.profile.bio}</div>
-
-  </div>
-  );
-}
+    );
+  }
