34efdf11d833e74774873dc8d5d9dddf8d47182d
Birch Tree <josiasmichael@gmail.com>
2023-06-05 02:00:14 +0000

    should correctly migrate

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -12,7 +12,7 @@ VALUES
   (
     (SELECT uuid from public.users WHERE users.handle = 'mysycry' LIMIT 1),
     'This was imported as seed data!',
-    current_timestamp 0 interval '10 day'
+    current_timestamp + interval '10 day'
   ),
   (
     (SELECT uuid from public.users WHERE users.handle = 'josiasmichael2' LIMIT 1),
