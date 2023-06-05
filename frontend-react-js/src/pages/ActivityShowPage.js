1e7a04b376fd8d52c9b1672724613c19acad91c7
Birch Tree <josiasmichael@gmail.com>
2023-06-05 02:43:16 +0000

    make changes

--- frontend-react-js/src/pages/ActivityShowPage.js
+++ frontend-react-js/src/pages/ActivityShowPage.js
@@ -1,14 +1,13 @@
-
 import './ActivityShowPage.css';
 import React from "react";
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
 import DesktopNavigation  from 'components/DesktopNavigation';
 import DesktopSidebar     from 'components/DesktopSidebar';
 import ActivityForm from 'components/ActivityForm';
 import ReplyForm from 'components/ReplyForm';
 import Replies from 'components/Replies';
-import ActivityItem from 'components/ActivityItem'
+import ActivityShowItem from 'components/ActivityShowItem'
 
 import {get} from 'lib/Requests';
 import {checkAuth} from 'lib/CheckAuth';
@@ -23,6 +22,11 @@ export default function ActivityShowPage() {
   const dataFetchedRef = React.useRef(false);
   const params = useParams();
 
+	const navigate = useNavigate();
+	const goBack = () => {
+		navigate(-1);
+	}
+
   const loadData = async () => {
     const url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/@${params.handle}/status/${params.activity_uuid}`
     get(url,{
@@ -33,7 +37,7 @@ export default function ActivityShowPage() {
       }
     })
   }
-
+  
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
@@ -46,7 +50,7 @@ export default function ActivityShowPage() {
   let el_activity
   if (activity !== null){
     el_activity = (
-      <ActivityItem
+      <ActivityShowItem 
         expanded={true}
         setReplyActivity={setReplyActivity}
         setPopped={setPoppedReply}
@@ -66,11 +70,13 @@ export default function ActivityShowPage() {
         <ReplyForm 
           activity={replyActivity} 
           popped={poppedReply} 
+          setReplies={setReplies}
           setPopped={setPoppedReply} 
         />
         <div className='activity_feed'>
-          <div className='activity_feed_heading'>
-          <div className='title'>Crud</div>
+          <div className='activity_feed_heading flex'>
+          <div className="back" onClick={goBack}>&larr;</div>	
+            <div className='title'>Crud</div>
           </div>
           {el_activity}
           <Replies
